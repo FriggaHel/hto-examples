b1f21656168ba65cd2cb789ea19258e066562d41
Felix P <felix@saucelabs.com>
2023-04-03 13:59:00 +0200

    Move artifacts folder

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: false
+    supportFile: false,
+    screenshotsFolder: 'cypress/artifacts/screenshots',
+    videosFolder: 'cypress/artifacts/videos',
   },
 });
